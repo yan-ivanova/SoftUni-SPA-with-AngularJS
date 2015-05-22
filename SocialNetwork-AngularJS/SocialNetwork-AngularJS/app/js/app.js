bc2bea578aef60cdfa3de3b34fd88e90196f8aac
yan-ivanova <yana.st.ivanova@gmail.com>
2015-05-22 18:40:27 +0300

    create index.html and app.js

--- SocialNetwork-AngularJS/SocialNetwork-AngularJS/app/js/app.js
+++ SocialNetwork-AngularJS/SocialNetwork-AngularJS/app/js/app.js
@@ -2,3 +2,24 @@
 
 
 var app = angular.module('SocialNetwork', ['ngRoute', 'ngResource']);
+
+app.constant('baseServiceUrl', 'http://softuni-social-network.azurewebsites.net/api');
+
+app.config(function ($routeProvider) {
+
+    $routeProvider.when('/', {
+        templateUrl: 'templates/home.html',
+        controller: 'HomeController'
+    });
+
+    $routeProvider.when('/login', {
+        templateUrl: 'templates/login.html',
+        controller: 'LoginController'
+    });
+
+    $routeProvider.when('/register', {
+        templateUrl: 'templates/register.html',
+        controller: 'RegisterController'
+    });
+
+});
